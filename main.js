3bd832d13f510a947d1a2e46a36fe59d8a71c224
Burdiugh <78913063+Burdiugh@users.noreply.github.com>
2026-01-22 20:38:34 +0200

    exercise 1st version

--- main.js
+++ main.js
@@ -113,3 +113,80 @@ createApp({
 
 }).mount('#app')
 
+createApp({
+    data(){
+        return{
+            title: 'Studying Vue.js plan',
+            position: 0,
+            stagesButtons: [
+                {label: '1', dedication: 'Basics', description: 'basics description', isActive: true, isColored: true},
+                {label: '2', dedication: 'Components', description: 'Components description', isActive: false, isColored: false},
+                {label: '3', dedication: 'Router', description: 'router description', isActive: false, isColored: false},
+                {label: '4', dedication: 'Vuex', description: 'Vuex description', isActive: false, isColored: false},
+                {label: '5', dedication: 'Composition', description: 'Composition description', isActive: false, isColored: false},
+            ],
+        }
+    },
+    // template: `
+        
+    // `,
+    methods: {
+        moveBack(){
+            if(this.position === 0) return;
+            else{
+                this.stagesButtons[this.position].isActive = false;
+                this.stagesButtons[this.position].isColored = false;
+                this.stagesButtons[this.position-1].isActive = true;
+                this.position--;
+            }
+        },
+        moveForward(){
+            if(this.position===this.stagesButtons.length-1) return; 
+            else{
+                 this.position++;
+                 this.stagesButtons[this.position-1].isActive = false;
+                 this.stagesButtons[this.position].isActive = true;
+                 this.stagesButtons[this.position].isColored = true;
+                  
+            }
+        },
+        onStageHandler(index){
+           
+            switch (index) {
+                case 0: { 
+                    
+                    if(!this.stagesButtons[index+1].isColored){return}
+                    else{
+                        for (let i = this.stagesButtons.length; i>1; i--){
+                            this.stagesButtons[i-1].isColored=false; 
+                            this.stagesButtons[i-1].isActive=false;
+                        }
+                        this.stagesButtons[index].isActive = true;
+                        this.position = 0;
+                    }
+
+                }
+                break;
+                case 1: { console.log(index);}
+                break;
+                case 2: { console.log(index);}
+                break;
+                case 3: { console.log(index);}
+                break;
+                case 4: { 
+                        for (let i = 0; i<this.stagesButtons.length; i++){
+                            this.stagesButtons[i].isColored=true; 
+                            this.stagesButtons[i].isActive=false;
+                        }
+                        this.stagesButtons[index].isActive = true;
+                        this.position = 4;
+                }
+                break;
+                
+            }
+
+        }
+    },
+    
+}).mount('#practiceapp')
+    
